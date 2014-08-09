258542f215a7d616abf041810e3d08124d95feaa
Peter Hellberg <peter@c7.se>
2014-08-09 23:33:13 +0200

    Connecting to MongoDB

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"log"
 	"os"
+
+	"gopkg.in/mgo.v2"
 
 	"github.com/peterhellberg/generator-generator/server"
 )
@@ -11,11 +14,31 @@ const (
 )
 
 func main() {
+	// Open a connection to MongoDB
+	log.Println("Connecting to MongoDB…")
+	sess, err := mgo.Dial(mongoURL())
+	if err != nil {
+		log.Printf("Can't connect to MongoDB, go error %v\n", err)
+		os.Exit(1)
+	}
+	defer sess.Close()
+
 	port := getenv("PORT", defaultPort)
-	err := server.ListenAndServe(os.Stdout, ":"+port)
+	err = server.ListenAndServe(os.Stdout, ":"+port)
 	if err != nil {
 		panic(err)
 	}
+}
+
+func mongoURL() (url string) {
+	url = os.Getenv("MONGOHQ_URL")
+
+	if url == "" {
+		log.Println("ENV variable MONGOHQ_URL not set!")
+		os.Exit(1)
+	}
+
+	return
 }
 
 func getenv(key, fallback string) string {
