fa0578cf0ad3aa08751a53d1114e2f9e2e82aa59
Peter Hellberg <peter@c7.se>
2014-08-18 02:21:32 +0200

    Made size of favicon configurable with query param

--- server/favicon.go
+++ server/favicon.go
@@ -8,21 +8,39 @@ import (
 	"image/draw"
 	"image/png"
 	"net/http"
+	"strconv"
 )
 
 var (
-	blue = color.RGBA{0x15, 0x8C, 0xBA, 0xFF}
-	icon = pngBytes(fill(image.NewRGBA(image.Rect(0, 0, 16, 16)), blue))
+	blue   = color.RGBA{0x15, 0x8C, 0xBA, 0xFF}
+	icon16 = iconBytes(16, blue)
 )
 
 func favicon(w http.ResponseWriter, r *http.Request) {
-	w.Write(icon)
+	s, err := strconv.Atoi(r.URL.Query().Get("s"))
+
+	// Return default icon if error,
+	// less than 16 px or above 1024 px
+	if err != nil || s <= 16 || s > 1024 {
+		w.Write(icon16)
+		return
+	}
+
+	w.Write(iconBytes(s, blue))
 }
 
 func fill(i *image.RGBA, c color.RGBA) *image.RGBA {
 	draw.Draw(i, i.Bounds(), &image.Uniform{c}, image.ZP, draw.Src)
 
 	return i
+}
+
+func iconBytes(s int, c color.RGBA) []byte {
+	return pngBytes(iconImage(s, c))
+}
+
+func iconImage(s int, c color.RGBA) *image.RGBA {
+	return fill(image.NewRGBA(image.Rect(0, 0, s, s)), blue)
 }
 
 func pngBytes(i *image.RGBA) []byte {
